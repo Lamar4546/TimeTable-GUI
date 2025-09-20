fa03a97ce8d325f128befa63bba0e6e873e7c0ad
Lamar Shaw <lamarshaw18@gmail.com>
2025-09-19 22:30:32 -0500

    timetable

---
+++ TimeTable.java
@@ -0,0 +1,14 @@
+public class TimeTable{
+  public String className;
+  public String classCode;
+  public String times;
+
+
+  public TimeTable(String className, String classCode, String times){
+    this.className = className;
+    this.classCode = classCode;
+    this.times = times;
+  }
+
+
+}
